6a9d32273b2997062354b0fffda14e0c35a7d3b4
ahmad-frehan <ahmad-frehan@futurecode-co.com>
2025-12-09 13:15:49 +0300

    init

--- cart/urls.py
+++ cart/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import CartApi,AddtoCartApi
+from .views import CartApi, AddtoCartApi, checkout
 urlpatterns = [
-    path('api/carts',CartApi.as_view(),name='carts'),
-    
+    # path('api/carts',CartApi.as_view(),name='carts'),
+
     path("add/", AddtoCartApi.as_view(), name="add-to-cart"),
+
+    path('', CartApi.as_view()),
+    path('checkout/', checkout),
 ]
--- cart/views.py
+++ cart/views.py
@@ -11,10 +11,14 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from django.shortcuts import get_object_or_404
-
+from orders.models import Order, OrderItem
 from cart.models import Cart, CartItem
 from cart.serializers import CartSerializer
+from orders.serializers import OrderSerializer
 from products.models import ProductVariant
+from django.db import transaction
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 
 class CartApi(APIView):
@@ -54,3 +58,44 @@ class AddtoCartApi(APIView):
             cart_item.save()
         serializer = CartSerializer(cart)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+@permission_classes[IsAuthenticated]
+def checkout(request):
+    user = request.user
+    variant_id = request.data.get("variant_id")
+    quantity = request.data.get("quantity")
+    try:
+        cart = Cart.objects.get(user=user)
+    except Cart.DoesNotExist:
+        return Response({"error": "Cart not found"}, status=status.HTTP_404_NOT_FOUND)
+    cart_item = cart.items.all()
+    if not cart_item:
+        return Response({"Error": "cart has not item"}, status=400)
+    with transaction.atomic():
+        for item in cart_item:
+            if item.variant.stock < item.quantity:
+                return Response({
+                    "error": f"Not enough stock for {item.varient.product.name}"
+                }, status=400)
+
+        order = Order.objects.create(
+            user=user,
+            address=user.default_address,
+            total_price=cart.total()
+        )
+        for item in cart_item:
+            orderitem = OrderItem.objects.create(
+                order=order,
+                variant_id=item.variant,
+                quantity=item.quantity,
+                price=item.variant.price
+            )
+
+            item.variant.stock -= item.quantity
+            item.variant.save()
+        cart.items.all().delete()
+
+    serializer = OrderSerializer(order)
+    return Response(serializer.data, status=201)
